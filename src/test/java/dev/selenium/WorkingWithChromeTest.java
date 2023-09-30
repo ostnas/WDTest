844c6139a1e07c8f3ead6d8f38789a9becb05719
ostnas <ostnas95@gmail.com>
2023-09-30 20:05:11 +0300

    Selenium WebDriver. First test added.

---
+++ src/test/java/dev/selenium/WorkingWithChromeTest.java
@@ -0,0 +1,38 @@
+package dev.selenium;
+
+import org.junit.jupiter.api.Test;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.time.Duration;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class WorkingWithChromeTest {
+    @Test
+    public void invokeBrowser() {
+        System.setProperty("webdriver.chrome.driver", "/Applications/chromedriver");
+
+        ChromeDriver driver = new ChromeDriver();
+        driver.get("https://www.selenium.dev/selenium/web/web-form.html");
+
+        String title = driver.getTitle();
+        assertEquals("Web form", title);
+
+        driver.manage().timeouts().implicitlyWait(Duration.ofMillis(500));
+
+        WebElement textBox = driver.findElement(By.name("my-text"));
+        WebElement submitButton = driver.findElement(By.cssSelector("button"));
+
+        textBox.sendKeys("Selenium");
+        submitButton.click();
+
+        WebElement message = driver.findElement(By.id("message"));
+        String value = message.getText();
+        assertEquals("Received!", value);
+
+        driver.quit();
+    }
+}
